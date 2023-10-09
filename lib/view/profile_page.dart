87019675870ac9ccdab83d3311615f2bf558771b
aanish37 <adhikariaanish37@gmail.com>
2023-10-09 23:34:21 +0545

    added profile image

--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -1,10 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../constant.dart';
+import '../widget/profile_menu_widget.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
+  final username = 'yubi';
+  final userEmail = 'adhikariyubraj894@gmail.com';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+    print(size);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Profile'),
+        backgroundColor: backgroundColor,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: size.height * 0.2,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(size.width * 0.92),
+                  child: Image.asset('asset/images/profile.png'),
+                ),
+              ),
+              Text(
+                username,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text(userEmail),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                  width: 200,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Edit Profile',
+                        style: TextStyle(color: borderColor),
+                      ))),
+              SizedBox(
+                height: 20,
+              ),
+              ProfileMenuWidget(
+                  title: 'Change Details', icon: Icons.edit, onPress: () {}),
+              ProfileMenuWidget(
+                  title: 'Reports', icon: Icons.edit_document, onPress: () {}),
+              ProfileMenuWidget(
+                  title: 'Settings', icon: Icons.settings, onPress: () {}),
+              ProfileMenuWidget(
+                title: 'Logout',
+                icon: Icons.logout,
+                onPress: () {},
+                textColor: Colors.red,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
