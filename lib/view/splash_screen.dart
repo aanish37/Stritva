07cb75a393dd0f6967f297d92e1df3dbf254924c
aanish37 <adhikariaanish37@gmail.com>
2023-10-15 00:04:46 +0545

    splash_screen work done

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:stritva/constant.dart';
 import '../view/home_page.dart';
-import '../view/intro/first.dart';
+import '../view/intro/second.dart';
 import '../model/user_data.dart';
 import 'package:provider/provider.dart';
+import 'package:easy_splash_screen/easy_splash_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,36 +16,44 @@ class SplashScreen extends StatefulWidget {
 }
 
 class SplashState extends State<SplashScreen> {
-  Future checkFirstSeen() async {
+  Future<Widget> checkFirstSeen() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool _seen = (prefs.getBool('seen') ?? false);
+    bool seen = (prefs.getBool('seen') ?? false);
 
-    if (_seen) {
-      return HomePage.id;
-    } else {
-      // Set the flag to true at the end of onboarding screen if everything is successfull and so I am commenting it out
-      return FirstPage.id;
-    }
+    return Future.delayed(const Duration(seconds: 3), () {
+      if (seen) {
+        Provider.of<UserData>(context, listen: false).getUserFromPrefs();
+        return HomePage();
+      } else {
+        // Set the flag to true at the end of the onboarding screen if everything is successful
+        // and return FirstPage()
+        return const SecondPage();
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: checkFirstSeen(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            return MaterialApp(
-              initialRoute: snapshot.data,
-              routes: {
-                FirstPage.id: (context) => FirstPage(),
-                HomePage.id: (context) => HomePage(),
-              },
-            );
-          }
-        });
+    final size = MediaQuery.of(context).size;
+    final height = size.height;
+
+    final width = size.width;
+
+    // Calculate the logo height and logo width based on screen size
+    final logoHeight = height * 0.65;
+    final logoWidth = width * 0.95;
+
+    return EasySplashScreen(
+      logo: Image.asset(
+        'asset/images/logo.png',
+        height: logoHeight,
+        width: logoWidth,
+        fit: BoxFit.contain,
+      ),
+      backgroundColor: backgroundColor,
+      showLoader: true,
+      loadingText: const Text("Loading..."),
+      futureNavigator: checkFirstSeen(),
+    );
   }
 }
